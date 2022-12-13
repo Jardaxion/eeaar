3180bad5dd0cd281e5d8397f37eb73dc15137720
Jardaxion <bainthecreator21@gmail.com>
2022-12-13 22:27:23 +0700

    a

--- js/main.js
+++ js/main.js
@@ -15,47 +15,80 @@ $(document).ready(() => {
 
     //Слайдеры
     //Начальный слайдер
-    $('.sliderMain__content').slick({
-        nextArrow: '.sliderMain__rightArrow',
-        prevArrow: '.sliderMain__leftArrow',
-        customPaging : function(slider, i) {
-            return '<div class="slider__dots"></div>';
+    // $('.sliderMain__content').slick({
+    //     nextArrow: '.sliderMain__rightArrow',
+    //     prevArrow: '.sliderMain__leftArrow',
+    //     customPaging : function(slider, i) {
+    //         return '<div class="slider__dots"></div>';
+    //     },
+    //     dots: true,
+    //     autoplay: true,
+    //     speed: 650,
+    //     autoplaySpeed: 5000,
+    //     slidesPerRow: 1,
+    //     infinite: true,
+    //     swipe: false
+    // });
+    let sldMain = new Swiper('.sliderMain__content', {
+        loop: true,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.sliderMain__rightArrow',
+            prevEl: '.sliderMain__leftArrow',
         },
-        dots: true,
-        autoplay: true,
-        speed: 650,
-        autoplaySpeed: 5000,
-        slidesPerRow: 1,
-        infinite: true,
-        swipe: false
-    });
+        pagination: {
+            el: '.sliderMain__dots',
+        },
+    })
     //Отзывы
-    $('.reviews__reviews').slick({
-        arrows: true,
-        dots: false,
-        autoplay: false,
-        nextArrow: '.reviews__rightArrow',
-        prevArrow: '.reviews__leftArrow',
-        slidesToShow: 2,
+    // $('.reviews__reviews').slick({
+    //     arrows: true,
+    //     dots: false,
+    //     autoplay: false,
+    //     nextArrow: '.reviews__rightArrow',
+    //     prevArrow: '.reviews__leftArrow',
+    //     slidesToShow: 2,
+    // })
+    let sldRev = new Swiper('.reviews__swiper', {
+        loop: true,
+        slidesPerView: 2,
+        spaceBetween: 28,
+        navigation: {
+            nextEl: '.reviews__rightArrow',
+            prevEl: '.reviews__leftArrow',
+        },
     })
     //Слайдер как происходит получение услуги
-    $('.questions__content').slick({
-        dots: false,
-        autoplay: false,
-        nextArrow: '.questions__rightArrow',
-        prevArrow: '.questions__leftArrow',
-        slidesToShow: 3,
-        infinite: false,
-        responsive: [
-            {
-                breakpoint: 961,
-                settings: {
-                    slidesToShow: 1
-                },
+    // $('.questions__content').slick({
+    //     dots: false,
+    //     autoplay: false,
+    //     nextArrow: '.questions__rightArrow',
+    //     prevArrow: '.questions__leftArrow',
+    //     slidesToShow: 3,
+    //     infinite: false,
+    //     responsive: [
+    //         {
+    //             breakpoint: 961,
+    //             settings: {
+    //                 slidesToShow: 1
+    //             },
+    //         }
+    //     ]
+    // })
+    let sldQuest = new Swiper('.questions__content', {
+        loop: false,
+        slidesPerView: 1,
+        spaceBetween: 17,
+        navigation: {
+            nextEl: '.questions__rightArrow',
+            prevEl: '.questions__leftArrow',
+        },
+        breakpoints: {
+            960: {
+                slidesPerView: 3
             }
-        ]
+        }
     })
-
     //Прокручиваемая строка с картиками
     $('.partners__content').marquee({
         duration: 10000,
